c3295365016b00ec1f1708ae8809a84b4190a584
nidhogg <ismail.elouafiq@telecom-bretagne.eu>
2014-04-25 00:17:33 +0200

    lvl up

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from deduper import Deduper
-from config import COMPARATORS, file_name, fields
+from config import file_name, fields
 from file_manager import FileManager
 
 deduper = Deduper(file_name['setting'], fields)
@@ -12,3 +12,4 @@ data_sample = data_file.data_dict
 deduper.train(data_sample, True, file_name['training'])
 
 deduper.start_active_labeling(file_name['training'], file_name['setting'])
+
